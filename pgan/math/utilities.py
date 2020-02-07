8bc4cd42ff107c5a9cacaed971e33c6182e67f4f
Bryan Riel <bryanvriel@gmail.com>
2020-02-07 16:25:34 +0800

    Adding custom leaky ELU activation function.

--- pgan/math/utilities.py
+++ pgan/math/utilities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import pgan.tensorflow as tf
+
 def normalize(x, xmin, xmax):
     """
     Normalize data to be roughly in the range of [-1, 1].
@@ -11,5 +13,14 @@ def identity(x, *args):
     Do nothing. Simply return input.
     """
     return x
+
+def leaky_elu(x, slope=0.2):
+    """
+    Custom activation function that combines leaky ReLU + exponential LU.
+    """
+    cond = tf.greater_equal(x, tf.constant(0.0))
+    pos_values = x
+    neg_values = tf.exp(x) + slope * x - tf.exp(0.0)
+    return tf.where(cond, pos_values, neg_values)
 
 # end of file
