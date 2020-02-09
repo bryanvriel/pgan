462a5d109585268d46a85c3399613b1dcc94e0e9
Bryan Riel <bryanvriel@gmail.com>
2020-02-09 10:07:31 +0800

    Adding operators for Gaussian smoothing and gradients in 1d.

--- pgan/math/image.py
+++ pgan/math/image.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
 import numpy as np
-import tensorflow as tf
+import pgan.tensorflow as tf
 
 def build_sobel_filters():
     """
--- pgan/math/utilities.py
+++ pgan/math/utilities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pgan.tensorflow as tf
 
 def normalize(x, xmin, xmax):
@@ -22,5 +23,41 @@ def leaky_elu(x, slope=0.2):
     pos_values = x
     neg_values = tf.exp(x) + slope * x - tf.exp(0.0)
     return tf.where(cond, pos_values, neg_values)
+
+def gaussian_smoothing_kernel(win_size=9):
+
+    # Construct kernel
+    if win_size == 5:
+        kernel = np.array([0.06136, 0.24477, 0.38774, 0.24477, 0.06136])
+    elif win_size == 9:
+        kernel = np.array([0.029, 0.067, 0.124, 0.179, 0.202, 0.179, 0.124, 0.067, 0.029])
+    else:
+        raise ValueError('Unsupported window size for Gaussian kernel.')
+    
+    # Normalize
+    kernel /= np.sum(kernel)
+
+    # Return as tensorflow tensor with shape [filter_width, in_channels, out_channels]
+    return tf.convert_to_tensor(kernel.reshape(-1, 1, 1).astype(np.float32))
+
+def smoothe1d(x, filters):
+    """
+    Performs 1D convolution for smoothing.
+    """
+    # Reshape input to include channels dimension 
+    xn = tf.expand_dims(x, axis=-1)
+    
+    # Perform 1D convolution (NOTE: result will have edge effects)
+    out = tf.nn.conv1d(xn, filters=filters, stride=1, padding='SAME', data_format='NWC')
+
+    # Squeeze dimensions and return
+    return out[:, :, 0]
+
+def grad1d(x, D):
+    """
+    Multiplies finite difference operator with tensor.
+    """
+    return tf.einsum('ij,kj->ki', D, x)
+
 
 # end of file
